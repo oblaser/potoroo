2f00f76ee3c837669023f7619ad93158b917af85
Oliver Blaser <kontakt@oblaser.ch>
2021-02-17 11:40:46 +0100

    expanded processor_test

---
+++ test/system/processor_test_src/nBytes.js
@@ -0,0 +1,12 @@
+/*
+
+a file with n * pbSize bytes to test the boundary conditions of the processor
+
+*/
+
+function asdfghjkl()
+{
+    let c = Math.sqrt(a*a + b*b);
+    console.log('c = ' + c);
+    console.log('1234');
+}
